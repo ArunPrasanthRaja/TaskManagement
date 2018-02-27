771fe25dc56fc2a857dd6eb702334c3e6cefa6a2
ArunPrasanthRaja <arunprasanth25@gmail.com>
2018-02-27 22:23:58 +0530

    first Commit

---
+++ TaskManagement/src/main/java/com/cts/MSTask/MsTaskApplication.java
@@ -0,0 +1,12 @@
+package com.cts.MSTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTaskApplication.class, args);
+	}
+}
